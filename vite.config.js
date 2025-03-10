b7e346dd133b4cd81947a1d2b4e27383cd1052a5
Shubho-R-gif <penguinr4@gmail.com>
2025-03-10 12:28:56 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensures correct base path
-  build: {
-    outDir: "dist", // Ensures Vercel knows where the build output is
-  },
-  server: {
-    historyApiFallback: true, // Ensures client-side routing works
-  }
 })
